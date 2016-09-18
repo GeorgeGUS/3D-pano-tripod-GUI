ef3ec09a51e7aaa1e8c05ed750f2b6c61d0a3de6
GeorgeGUS <georgegus1@gmail.com>
2016-09-18 23:25:52 +0300

    Delete old functions, animated section rotation by button with timer

--- top-secret-project/elements/spinner/spinner.js
+++ top-secret-project/elements/spinner/spinner.js
@@ -1,41 +1,88 @@
-'use strict';
+//'use strict';
+/* jslint node: true */
 
-var axis = document.querySelectorAll(".axis");
 
-axis.forEach(function (item, i, arr) {
-  var rotate = item.querySelector(".rotate_control"), //регулятор угла поворота
-    spinner = item.querySelector(".spinner_segment"), //вращающийся сегмент
-    numOfRevo = item.querySelector(".num_of_revo"); //количество оборотов
+var document,
+  window,
+  axisAll = document.querySelectorAll(".axis"),
+  axisX = document.querySelector("#X-axis-spinner"),
+  axisY = document.querySelector("#Y-axis-spinner");
+
+function rotateAxisBySlider(axis) {
+  'use strict';
+  var rotate = axis.querySelector(".rotate_control"), //регулятор угла поворота
+    spinner = axis.querySelector(".spinner_segment"), //вращающийся сегмент
+    numOfRevo = axis.querySelector(".num_of_revo"); //количество оборотов
   rotate.addEventListener('input', function () {
-    spinner.style.cssText = "transform: rotate(" + (rotate.value * numOfRevo.value - 90) + "deg)";
+    spinner.style.transform = "rotate(" + (rotate.value * numOfRevo.value - 90) + "deg)";
   }, false);
-});
+}
 
+rotateAxisBySlider(axisX);
+rotateAxisBySlider(axisY);
 
-
-function timeIntervalRotation(angle) {
-  var i,
-    intervalID,
-    rotate = document.querySelector(".rotate_control"), //регулятор угла поворота
-    spinner = document.querySelector(".spinner_segment");
-  for (i = 0; i < 4; i++) {
-    spinner.style.cssText = "transform: rotate(" + (angle * i - 90) + "deg)";
-    window.setInterval(i, 1000);
-//    window.clearInterval(intervalID);
+function rotateAxisByLoop(numRev) {
+  'use strict';
+  var i;
+  //    var axis = document.querySelector("#X-axis-spinner"),
+  //      spinner = axis.querySelector(".spinner_segment");
+  for (i = 1; i <= numRev; i += 1) {
+    document.querySelector(".spinner_segment").style.transform = "rotate(" + (45 * i - 90) + "deg)";
   }
 }
 
+function wideAngleSegment(angleWidth) {
+  'use strict';
+  axisAll.forEach(function (item, i, arr) {
+    var wide = item.querySelector(".wide_control"), //регулятор угла обзора
+      width_left = item.querySelector(".spinner_segment_before"),
+      width_right = item.querySelector(".spinner_segment_after");
+    width_left.style.cssText = "transform: rotate(" + (45 + (180 - angleWidth) / 2) + "deg)";
+    width_right.style.cssText = "transform: rotate(" + (45 - (180 - angleWidth) / 2) + "deg)";
+  });
+}
 
-//function rotateSegment(a) {
-//  axis.forEach(function (item, i, arr) {
-//    var rotate = item.querySelector(".rotate_control"), //регулятор угла поворота
-//      spinner = item.querySelector(".spinner_segment"); //вращающийся сегмент
-//    //      numOfRevo = item.querySelector(".num_of_revo"); //количество оборотов
-//    
-//    spinner.style.cssText = "transform: rotate(" + (a - 90) + "deg)";
-//         
-//  });
-//}
+
+var startBtn = document.querySelector("#startBtn"),
+  spinner = document.querySelector(".spinner_segment");
+
+startBtn.onclick = function () {
+  'use strict';
+  var angleWidth = 45;
+  wideAngleSegment(angleWidth); // задать ширину угла сегмента
+  var start = Date.now(), // сохранить время начала
+    timer = setInterval(function () {
+      // вычислить сколько времени прошло с начала анимации
+      var timePassed = Date.now() - start;
+
+      spinner.style.transform = "rotate(" + (angleWidth * (timePassed / 1000) - 90) + "deg)";
+      document.querySelector("#timerID").innerHTML = timePassed / 1000;
+
+      if (timePassed >= 8000) {
+        clearInterval(timer);
+//        spinner.style.transform = "rotate(-90deg)";
+        return;
+      }
+      
+    }, 250);
+};
+
+
+
+
+
+
+
+
+function rotateAxisByFunction(a) {
+  'use strict';
+  axisAll.forEach(function (item, i, arr) {
+    var rotate = item.querySelector(".rotate_control"), //регулятор угла поворота
+      spinner = item.querySelector(".spinner_segment"); //вращающийся сегмент
+    //      numOfRevo = item.querySelector(".num_of_revo"); //количество оборотов    
+    spinner.style.cssText = "transform: rotate(" + (a - 90) + "deg)";
+  });
+}
 
 
 
@@ -46,7 +93,10 @@ function timeIntervalRotation(angle) {
 //rotateSegment();
 //wideAngleSegment();
 
-axis.forEach(function (item, i, arr) {
+
+
+axisAll.forEach(function (item, i, arr) {
+  'use strict';
   var wide = item.querySelector(".wide_control"), //регулятор угла обзора
     width_left = item.querySelector(".spinner_segment_before"),
     width_right = item.querySelector(".spinner_segment_after");
@@ -56,12 +106,4 @@ axis.forEach(function (item, i, arr) {
   }, false);
 });
 
-function wideAngleSegment(angleWidth) {
-  axis.forEach(function (item, i, arr) {
-    var wide = item.querySelector(".wide_control"), //регулятор угла обзора
-      width_left = item.querySelector(".spinner_segment_before"),
-      width_right = item.querySelector(".spinner_segment_after");
-    width_left.style.cssText = "transform: rotate(" + (45 + (180 - angleWidth) / 2) + "deg)";
-    width_right.style.cssText = "transform: rotate(" + (45 - (180 - angleWidth) / 2) + "deg)";
-  });
-}
+
